812b9dcb2ff6729255439ccd5fb397c3226b6370
MixterOne <matheusisaias879@gmail.com>
2022-07-15 12:22:26 -0300

    Adicionando Tarefas ao Kanban

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
 import Navbar from './components/navbar/Navbar';
-import TaskList from './components/tasklist/TaskList'
+import TaskList from './components/tasklist/TaskList';
+
+const task = {
+  id: 0,
+  title: 'Nova Tarefa',
+  state: 'pendente'
+}
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1
+  return idAcc
+}
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    }
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask]
+    })
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title="Pendente" />
+      <div className="container">
+        <div className="bx-1">
+        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
+        </div>
+        <div className="bx-2">
         <TaskList title="Fazendo" />
+        </div>
+        <div className="bx-3">
         <TaskList title="Completo" />
+        </div>  
       </div>
     </div>
   );
